e55281d45b254b124e7337ea1c9b9c7963d06a3a
azbito <azbitomsg@proton.me>
2024-05-29 08:53:33 -0300

    feat: delete users

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
-import http from 'node:http'
-import { json } from './middlewares/json.js'
-import { routes } from './middlewares/routes.js'
+import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { routes } from './middlewares/routes.js';
 
-
-const server =  http.createServer(async (req, res)=> {
-    const { method, url } = req
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req;
     
-    await json(req, res)
+    await json(req, res);
 
     const route = routes.find(route => {
-        return method == route.method && url == route.path
-    })
+        return method === route.method && route.path.test(url);
+    });
 
     if (route) {
-        return route.handler(req, res)
+        const routeParams = req.url.match(route.path);
+        req.params = {...routeParams.groups}; // Adding params to the request object for easy access
+        
+        console.log(routeParams);
+        
+        return route.handler(req, res);
     }
 
-    return res.writeHead(404).end() // error code
-})
+    return res.writeHead(404).end(); // error code
+});
 
-server.listen(3333)
+server.listen(3333);
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,8 @@
-// /users/:id
 export function buildRoutePath(path) {
-    const routeParametersRegex = /:([a-zA-Z0-9]+)/g
-
-    console.log(Array.from(path.matchAll(routeParametersRegex)))
+    const routeParametersRegex = /:([a-zA-Z]+)/g;
+    const pathWithParams = path.replace(routeParametersRegex, '(?<$1>[a-z0-9\\-_]+)');
+    
+    const pathRegex = new RegExp(`^${pathWithParams}$`);
+    
+    return pathRegex;
 }
